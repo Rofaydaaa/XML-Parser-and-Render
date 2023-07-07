cb8bf0db6a5529eea2ecd81bf1190f1d24d071c3
Rofayda <roufybassem@gamil.com>
2023-07-07 17:43:53 +0300

    Use httpClientFactory to create the http client

--- XMLParser/Pages/Index.cshtml.cs
+++ XMLParser/Pages/Index.cshtml.cs
@@ -1,44 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using System;
 using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
 
 namespace XMLParser.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
         public List<ItemDetails> ItemsDetails { get; set; } = new List<ItemDetails>();
 
-        public void OnGet()
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
         {
-            XmlDocument xmlDoc = new XmlDocument();
-            using (var client = new HttpClient())
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
             {
-                var response = client.GetAsync("http://scripting.com/rss.xml").Result;
+                var httpClient = _httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync("http://scripting.com/rss.xml");
+                XmlDocument xmlDoc = new XmlDocument();
+
                 if (response.IsSuccessStatusCode)
                 {
-                    var xmlContent = response.Content.ReadAsStringAsync().Result;
+                    var xmlContent = await response.Content.ReadAsStringAsync();
                     xmlDoc.LoadXml(xmlContent);
-                }
-
-                var itemsNode = xmlDoc.SelectNodes("rss/channel/item");
-
-                if (itemsNode != null)
-                {
-                    foreach (XmlNode itemNode in itemsNode)
+                    foreach (XmlNode itemNode in xmlDoc.SelectNodes("rss/channel/item"))
                     {
-                        var itemDetails = new ItemDetails
-                        {
-                            Description = itemNode.SelectSingleNode("description")?.InnerText,
-                            PubDate = itemNode.SelectSingleNode("pubDate")?.InnerText,
-                            Link = itemNode.SelectSingleNode("link")?.InnerText,
-                            Guide = itemNode.SelectSingleNode("guid")?.InnerText,
-                            Source = itemNode.SelectSingleNode("source")?.InnerText
-                        };
+                        var itemDetails = new ItemDetails();
+                        itemDetails.Description = itemNode.SelectSingleNode("description")?.InnerText;
+                        itemDetails.PubDate = itemNode.SelectSingleNode("pubDate")?.InnerText;
+                        itemDetails.Link = itemNode.SelectSingleNode("link")?.InnerText;
+                        itemDetails.Guide = itemNode.SelectSingleNode("guid")?.InnerText;
                         ItemsDetails.Add(itemDetails);
                     }
+                    return Page();
                 }
+                else
+                {
+                    _logger.LogError("Unsuccessful status code");
+                    return RedirectToPage("/Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while processing the request.");
+                return RedirectToPage("/Error");
             }
         }
 
@@ -51,6 +63,5 @@ namespace XMLParser.Pages
         public string PubDate { get; set; }
         public string Link { get; set; }
         public string Guide { get; set; }
-        public String Source { get; set; }
     }
 }
